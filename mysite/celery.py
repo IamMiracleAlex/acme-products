dafbbc723d17cb0135e12c7400f82a8857255cbd
Miracle <collinsalex50@gmail.com>
2022-01-30 21:47:23 +0100

    troubleshooting celery

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,21 +1,23 @@
 import os
 
+from django.conf import settings
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 app = Celery('mysite')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(
+    broker_url = settings.CELERY_BROKER_URL,
+    timezone='Africa/Lagos',
+    enable_utc=True,
+)
 app.autodiscover_tasks()
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+print(app.conf)
 
 
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
-
-# crontabs
-app.conf.update(
-    timezone='Africa/Lagos',
-    enable_utc=True,
-)
