5df43ce54897e2a5a7db23eee4c22fbec099df7b
Miracle <collinsalex50@gmail.com>
2022-01-30 23:41:02 +0100

    troubleshooting celery again

--- mysite/celery.py
+++ mysite/celery.py
@@ -6,13 +6,13 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-app = Celery('mysite')
-app.conf.update(
-    broker_url = settings.CELERY_BROKER_URL,
-    result_backend = settings.CELERY_BROKER_URL,
-    timezone='Africa/Lagos',
-    enable_utc=True,
-)
+app = Celery('mysite', broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_BROKER_URL)
+# app.conf.update(
+#     broker_url = settings.CELERY_BROKER_URL,
+#     result_backend = settings.CELERY_BROKER_URL,
+#     timezone='Africa/Lagos',
+#     enable_utc=True,
+# )
 app.autodiscover_tasks()
 # app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 print(app.conf)
