4d361452e2b958af19dad6a6d58d6826abf203ef
Miracle <collinsalex50@gmail.com>
2022-02-04 15:36:52 +0100

    remove chunks

--- products/views.py
+++ products/views.py
@@ -263,14 +263,16 @@ def get_uploaded_file(request):
         reader = csv.reader(f)
         next(reader)
         reader_list = list(reader)
-        count = len(reader_list)
-        step = 2000
+        process_task.delay(reader_list)
+
+        # count = len(reader_list)
+        # step = 2000
     
-        for i in range(0, count, step):
-            chunks = reader_list[i:i+step]
-            # print(chunks)
-            process_task.delay(chunks)
-            time.sleep(50)
-        # process_task(reader_list)
+        # for i in range(0, count, step):
+        #     chunks = reader_list[i:i+step]
+        #     # print(chunks)
+        #     process_task.delay(chunks)
+        #     time.sleep(50)
+        # # process_task(reader_list)
 
     return JsonResponse({'msg': 'Processing your CSV file'})
